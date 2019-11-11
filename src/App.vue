049ce3c71f8597eb13f75554e62d5851c09d116d
Bob Bol <bob@voorhoede.nl>
2019-11-11 16:26:53 +0100

    Welcome dialog :: needs proper text

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,22 @@
     <v-content>
       <app-map />
     </v-content>
+
+    <welcome-dialog />
+
   </v-app>
 </template>
 
 <script>
 import AppMap from '@/components/app-map';
 import AppSidebar from '@/components/app-sidebar';
+import WelcomeDialog from '@/components/welcome-dialog';
 
 export default {
   components: {
     AppMap,
-    AppSidebar
+    AppSidebar,
+    WelcomeDialog
   }
 };
 </script>
