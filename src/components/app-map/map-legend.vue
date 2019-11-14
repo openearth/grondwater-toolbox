a45f080d527c97851c1d473a5f5810bad944d037
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-11-14 16:54:00 +0100

    add legend to map

---
+++ src/components/app-map/map-legend.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="map-legend">
+    <img :src="imgUrl" />
+  </div>
+</template>
+
+<script>
+import legendUrl from '@/lib/legend-url'
+
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    layer: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    imgUrl() {
+      return legendUrl({
+        url: this.url,
+        layer: this.layer
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.map-legend {
+  position: absolute;
+  left: 1rem;
+  bottom: 1rem;
+  z-index: 100;
+}
+</style>
---
+++ src/lib/legend-url.js
@@ -0,0 +1,13 @@
+import geoserverUrl from './geoserver-url';
+
+export default function ({ layer, url }) {
+  return geoserverUrl({
+    request: 'GetLegendGraphic',
+    service: 'WMS',
+    format: 'image/png',
+    width: 40,
+    height: 40,
+    layer,
+    url
+  });
+}
