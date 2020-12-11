c09997f39e2b4fd854a6d3bbe12d9e02e67cdf36
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-12-11 13:24:40 +0100

    feat(load-button): zoom in to loaded selections

--- src/components/load-button.vue
+++ src/components/load-button.vue
@@ -14,41 +14,68 @@
 <script>
 import { mapActions, mapState } from 'vuex';
 import getLoadedFileContents from '@/lib/get-loaded-file-contents';
+import { featureCollection } from '@turf/helpers';
+import bbox from '@turf/bbox';
 
 export default {
   data() {
     return {
       showSnackBar: true,
+      selectionsCount: null,
+      shouldZoomIn: false
     };
   },
   computed: {
-    ...mapState('mapbox/selections', ['selections']),
+    ...mapState('mapbox', {
+      features: (state) => state.features,
+    }),
+  },
+  watch: {
+    features() {
+      if (this.features.length === this.selectionsCount && this.shouldZoomIn) {
+        const bounds = bbox(
+          featureCollection(
+            this.features.map((feature) => ({
+              geometry: feature.source.data,
+              type: 'Feature',
+            }))
+          )
+        );
+  
+        this.$root.map.fitBounds(bounds, { padding: 50 });
+
+        this.shouldZoomIn = false;
+      }
+    },
   },
   methods: {
     ...mapActions({
       showError: 'notifications/showError',
     }),
     async onFileInput(event) {
-      const { __draw} = this.$root.map;
-      const data = await getLoadedFileContents(event);  
+      const { __draw } = this.$root.map;
+      const data = await getLoadedFileContents(event);
 
       __draw.deleteAll();
       __draw.add({
-        type: "FeatureCollection",
-        features: data.selections.selections
+        type: 'FeatureCollection',
+        features: data.selections.selections,
       });
 
-      data.selections.selections.forEach(selection => {
+      data.selections.selections.forEach((selection) => {
         this.$store.commit('selections/add', selection);
         this.$store.dispatch('mapbox/getFeature', selection);
       });
 
       this.$store.dispatch('reset');
       this.$store.dispatch('loadProject', data);
+      this.selectionsCount = data.selections.selections.length;
 
       if (this.$route.name !== 'selection') {
         this.$router.push({ name: 'selection' });
       }
+
+      this.shouldZoomIn = true; 
     },
   },
 };
