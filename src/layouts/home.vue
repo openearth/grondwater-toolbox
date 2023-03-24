00e4ebdd41c556e0c80122c2d335eb5dc5df1489
IoannaMi <ioannamicha@hotmail.com>
2023-03-24 12:47:32 +0100

    add logos

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -11,7 +11,7 @@
           :to="{ name: 'home' }"
           class="white--text text-decoration-none"
         >
-          Grondwater Toolbox
+          Grondwater toolbox – 3D verkenning van de effecten van ingrepen
         </router-link>
       </v-toolbar-title>
       <v-spacer />
@@ -95,17 +95,29 @@
               <p class="text-body-1">De toolbox maakt het mogelijk om diverse ingrepen te simuleren via online interactie met de stationaire schematisatie in iMODFLOW van LHM4.2. Meer weten, neem dan contact op met <a href="mailto:helpdesk.nhi@deltares.nl?&subject=Online modelling feedback&body=Ik heb de online modelling tool gezien en getest en ik wil graag meer weten of heb opmerkingen over deze tool.">Heldesk NHI</a>.</p>
             </v-col>
           </v-row>
+          <v-row dense justify="center">
+            <v-col
+              sm="4"
+              md="2"
+              xl="1"
+            >
+              <img src="@/assets/img/deltares-logo.png" class="deltares_logo"/>
+            </v-col>
+            <v-col
+              sm="4"
+              md="2"
+              xl="1"
+            >
+              <img src="@/assets/img/Rijkswaterstaat_logo.png" class="rws_logo"/>
+            </v-col>
+          </v-row>
         </v-container>
       </section>
     </v-main>
     <v-footer color="primary" dark>
       <v-container>
         <v-row dense justify="center">
-          <v-col
-            sm="12"
-            md="10"
-            xl="6"
-          >
+          <v-col>
             <span class="text-body-2">&copy; Deltares - {{ currentYear }}</span>
             <span class="text-body-2">                                          RWS GPO - {{ currentYear }}</span>
           </v-col>
@@ -167,4 +179,14 @@
     object-fit: cover;
     height: 1000px;
   }
+  .deltares_logo{
+    max-width: 200px;
+    padding-top: 20px;
+    padding-right: 40px;
+
+  }
+  .rws_logo{
+    max-width: 200px;
+  }
+
 </style>
