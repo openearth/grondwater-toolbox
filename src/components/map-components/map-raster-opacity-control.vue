3b28a129faff4b59bc22e7592cbd9b7a8cfba737
IoannaMi <ioannamicha@hotmail.com>
2024-03-12 15:20:03 +0100

    small fixes

--- src/components/map-components/map-raster-opacity-control.vue
+++ src/components/map-components/map-raster-opacity-control.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="map-raster-opacity-control">
     <label class="map-raster-opacity-control__label text-body-2" for="opacity">
-      Opacity: {{ value }}%
+      Transparantie: {{ value }}%
     </label>
     <input
       class="map-raster-opacity-control__input"
---
+++ src/components/text-field-measure.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-card
+    class="pa-2 full-height d-flex"
+    outlined
+    tile
+    >
+        <v-text-field
+            v-model="difference"
+            class="hide-label"
+            type="number"
+            label="Verschil in rivierbodemhoogte (m)"
+            :rules="differenceRules"
+            :disabled="disabled"
+        />
+        </v-card>
+</template>
+<script>
+  export default {
+    props: {
+      differenceRules: Array,
+      disabled: Boolean,
+    },
+    data() { 
+      return {
+        difference: 1,
+      };
+    },
+    watch: {
+      difference(value) {
+        this.$emit('update-difference-value', value);
+      },
+    },
+  };
+
+</script>
+<style>
+.hide-label {
+  padding-top: 0;
+  margin-top: 0;
+}
+
+.hide-label label {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+}
+
+.hide-label .v-text-field__details,
+.hide-label .v-text-field__details .v-messages {
+  min-height: 0;
+}
+</style>
