559617b1f9f76981ce7a9d89be09eb8ddfe1d8c1
Bazottie <basdegreeuw@voorhoede.nl>
2024-12-11 12:40:39 +0100

    Adds a dropdown menu for output resolution for abstraction config

--- src/lib/get-abstraction-data.js
+++ src/lib/get-abstraction-data.js
@@ -1,6 +1,6 @@
 import geoServerUrl from './geoserver-url';
 
-const DATA_TEMPLATE = ({ area, coordinates, layer, abstraction }) =>
+const DATA_TEMPLATE = ({ area, coordinates, layer, outres, abstraction }) =>
   JSON.stringify({
     type: 'FeatureCollection',
     name: 'point',
@@ -13,6 +13,7 @@ const DATA_TEMPLATE = ({ area, coordinates, layer, abstraction }) =>
         type: 'Feature',
         properties: {
           fid: 1,
+          outres,
           layer,
           area,
           abstraction,
@@ -25,8 +26,8 @@ const DATA_TEMPLATE = ({ area, coordinates, layer, abstraction }) =>
     ],
   }, 0, false);
 
-export default async function getAbstractionData ({ area, coordinates, layer, abstraction }) {
-  const data = DATA_TEMPLATE({ area, coordinates, layer, abstraction });
+export default async function getAbstractionData ({ area, coordinates, layer, outres, abstraction }) {
+  const data = DATA_TEMPLATE({ area, coordinates, layer, outres, abstraction });
   const url = await geoServerUrl({
     url: process.env.VUE_APP_GEO_SERVER + '/wps',
     request: 'Execute',
