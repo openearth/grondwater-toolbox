58030e9592665c228e715b76e56d6cf0df7a78e0
Bazottie <basdegreeuw@voorhoede.nl>
2025-01-07 16:35:31 +0100

    Updates system layer button styling

--- src/components/map-components/map-levels.vue
+++ src/components/map-components/map-levels.vue
@@ -1,19 +1,52 @@
 <template>
   <div class="map-levels">
-    <v-form class="map-levels__form">
-      <v-row
-          v-for="level in Object.keys(_value)"
-          :key="level"
-          class="map-levels__row"
-      >
-        <v-checkbox
-            class="map-levels__item"
-            :aria-label="`Zichtbaarheid van de kaartlaag voor het ${_value[level].label} watersysteem`"
-            :label="_value[level].label"
-            v-model="_value[level].active"
-        />
-      </v-row>
-    </v-form>
+    <!--        <v-btn-->
+    <!--            color="#008fc5"-->
+    <!--            class="map-levels__item justify-start white&#45;&#45;text"-->
+    <!--            v-for="level in Object.keys(_value)"-->
+    <!--            :key="level"-->
+    <!--            @click="onToggle(level)"-->
+    <!--            :aria-label="`Zichtbaarheid van de kaartlaag voor het ${_value[level].label} watersysteem`"-->
+    <!--        >-->
+    <!--          <v-icon>-->
+    <!--            {{ _value[level].active ? 'mdi-eye' : 'mdi-eye-off'}}-->
+    <!--          </v-icon>-->
+    <!--          {{ _value[level].label }}-->
+    <!--        </v-btn>-->
+    <v-treeview
+        class="map-levels__tree"
+        expand-icon="mdi-chevron-down"
+        dense
+        :items=" [ {
+          label: 'Watersystemen',
+          children: Object.entries(this._value).flatMap(([ level, data ]) => ({ level, ...data })),
+        } ]"
+        open-on-click
+    >
+
+      <template #label="{ item }">
+        <v-btn
+            plain
+            color="#008fc5"
+            class="justify-start map-levels__button"
+            :key="item.label.toLowerCase()"
+            v-if="'active' in item"
+            @click="onToggle(item.level)"
+            :aria-label="`Zichtbaarheid van de kaartlaag voor het ${item.label} watersysteem`"
+        >
+          <v-icon>
+            {{ item.active ? 'mdi-eye' : 'mdi-eye-off' }}
+          </v-icon>
+          <span class="text-body-1">
+            {{ item.label }}
+          </span>
+        </v-btn>
+
+        <span v-else class="text-body-1 pr-2">
+          {{ item.label }}
+        </span>
+      </template>
+    </v-treeview>
   </div>
 </template>
 
@@ -36,41 +69,44 @@
         },
       },
     },
+    methods: {
+      onToggle(level) {
+        this._value[level].active = !this._value[level].active;
+      },
+    },
   };
 </script>
 
 <style>
 .map-levels {
   position: absolute;
+  border-radius: 4px;
+  overflow: hidden;
   z-index: 100;
-  top: 84px;
-  left: 22px;
-  flex-direction: column;
-  min-width: min-content;
-}
-
-.map-levels__form {
+  width: 180px;
+  top: 92px;
+  left: 10px;
   display: flex;
   flex-direction: column;
-}
-
-.map-levels__form > * {
-  margin-bottom: 2px;
-}
-
-.map-levels__row {
-  max-height: 24px;
-}
-
-.map-levels__item .v-input__slot{
-  background-color: #ffffff;
   box-shadow: 0 0 5px 2px rgba(0, 0, 0, .12);
-  padding: 0 6px;
-  border-radius: 4px;
 }
 
-.map-levels__item .v-input__slot, .map-levels__item .v-input__control, .map-levels__item .v-input {
-  min-width: min-content;
-  flex-grow: 1;
+.map-levels .v-treeview-node__level {
+  display: none;
+}
+
+.map-levels__tree .v-treeview-node {
+  position: relative;
+  background: white;
+}
+
+.map-levels__button {
+  position: static;
+}
+
+.map-levels__button::after {
+  content: '';
+  position: absolute;
+  inset: 0;
 }
 </style>
